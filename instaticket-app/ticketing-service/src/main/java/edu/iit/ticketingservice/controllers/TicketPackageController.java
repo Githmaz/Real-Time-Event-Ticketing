722cfeeba8b33fc5732b38ff7a20c5191d191b83
Githmaz <Githmaz1@gmail.com>
2025-01-27 02:45:04 +0530

    refactor: redesign event and ticket package structure - Enforce vendor ownership for events and their ticket packages.

--- instaticket-app/ticketing-service/src/main/java/edu/iit/ticketingservice/controllers/TicketPackageController.java
+++ instaticket-app/ticketing-service/src/main/java/edu/iit/ticketingservice/controllers/TicketPackageController.java
@@ -8,9 +8,11 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/ticket-packages")
 public class TicketPackageController {
 
@@ -18,10 +20,11 @@ public class TicketPackageController {
     private TicketPackageService ticketPackageService;
 
     // Create a new ticket package with tickets
+    @PreAuthorize("hasRole('VENDOR')")
     @PostMapping("/create")
     public ResponseEntity<ApiResponse<TicketPackage>> createTicketPackage(@Valid @RequestBody TicketPackageRequest request) {
-        TicketPackage createdPackage = ticketPackageService.createTicketPackage(request);
-        ApiResponse<TicketPackage> response = new ApiResponse<>(true, "Ticket package created with tickets", createdPackage);
+        ticketPackageService.createTicketPackage(request);
+        ApiResponse<TicketPackage> response = new ApiResponse<>(true, "Ticket package created with tickets", null);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
--- instaticket-app/ticketing-service/src/main/java/edu/iit/ticketingservice/dao/VendorEntity.java
+++ instaticket-app/ticketing-service/src/main/java/edu/iit/ticketingservice/dao/VendorEntity.java
@@ -2,11 +2,10 @@ package edu.iit.ticketingservice.dao;
 
 import jakarta.persistence.*;
 
+
 @Entity
 @Table(name = "vendor")
 public class VendorEntity  extends UsersEntity {
-
-
 
     public VendorEntity(Long id, String userId, String username, String password, String email, String name) {
         super(id, userId, username, password, email, name);
