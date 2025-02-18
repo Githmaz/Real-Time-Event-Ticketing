7ae6d80071598fdbab968765290757c8fd98d4b2
Githmaz <Githmaz1@gmail.com>
2025-02-18 23:27:25 +0530

    feat: Implemented TicketCart API for retrieving shopping cart data

---
+++ instaticket-app/ticketing-service/src/main/java/edu/iit/ticketingservice/dto/shoppingCart/TicketCart.java
@@ -0,0 +1,8 @@
+package edu.iit.ticketingservice.dto.shoppingCart;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public class shoppingCart {
+   
+}
